e2e60247482c94f507630ae3f5f079eff18bdc56
Joseph Dillon <joseph.dillon.522@gmail.com>
2021-10-06 12:15:08 -0400

    changed schedule and added a reminder

--- sendPerstat.js
+++ sendPerstat.js
@@ -54,6 +54,62 @@ sendPerstat = (app, users) => {
 };
 
 
+sendReminder = (app, users) => {
+    const rule = new schedule.RecurrenceRule();
+    rule.minute = [33];
+    rule.hour = 12;
+    rule.dayOfWeek = [new schedule.Range(1, 5)];
+
+    const job = schedule.scheduleJob(rule, () => {
+        console.log('---- Starting PERSTAT Solicitation ----');
+
+        users.forEach(user => {
+            if (!user.responded) {
+
+                console.log(`Sending Reminder Ping: ${user.name} / ${user.id}`);
+
+                app.client.chat.postMessage({
+                    channel: user.id,
+                    blocks: [
+                        {
+                            type: "header",
+                            text: {
+                                type: "plain_text",
+                                text: "------- 186 CPT - PERSTAT - FINAL REMINDER -------"
+                            }
+                        },
+                        {
+                            type: "section",
+                            text: {
+                                type: "mrkdwn",
+                                text: "Wake up! You haven't submitted your status yet\n\nThis is your last PERSTAT reminder before the report is sent"
+                            }
+                        },
+                        {
+                            type: "actions",
+                            elements: [
+                                {
+                                    type: "button",
+                                    text: {
+                                        type: "plain_text",
+                                        text: "I'm UP!"
+                                    },
+                                    action_id: "send_perstat"
+                                }
+                            ]
+                        },
+                        {
+                            type: "divider"
+                        },
+                    ],
+                    text: 'Last Reminder to submit your PERSTAT status!'
+                });
+            }
+        });
+    });
+}
+
+
 
 module.exports = {
     sendPerstat: sendPerstat
