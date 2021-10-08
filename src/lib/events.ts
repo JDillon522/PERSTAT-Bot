16ad60400a124df2ef79ac33a083cd09ca5d0549
Joseph Dillon <joseph.dillon.522@gmail.com>
2021-10-08 10:57:33 -0400

    make sure to send ack on an action

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -13,12 +13,12 @@ export const registerClickEvents = (app: App) => {
 }
 
 export const respondToButton = async ({body, ack, say}: SlackActionMiddlewareArgs) => {
+    await ack();
+
     // If there is a delay in the response from the client to the app then the client will resend the request (timeframe unknown)
     // This is to prevent double output on the client. We still need to make sure the submit ability is disabled after submission though
     const user = getUser(body.user.id);
     if (!user?.responded) {
-        await ack();
-
         await say(`I'm glad to hear it <@${body.user.id}>.\n\nNow go forth and do great things.\n\n"I give a shit about you" - SFC Boyce`);
 
         markUserAsPresent(body.user.id);
